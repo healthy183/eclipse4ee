84a2620ecf84eea759fdda559b95771506f2a2a2
梁健康 <liangjiankang-it@bestpay.com.cn>
2016-05-28 15:11:00 +0800

    添加局部变量get和set性能测试

--- javaSE/src/com/javaSE/visit/FinalClass.java
+++ javaSE/src/com/javaSE/visit/FinalClass.java
@@ -2,16 +2,74 @@ package com.javaSE.visit;
 
 public class FinalClass {
 
-	public static void main(String[] args) {
+	private final int i = 0;
 	
-		final String userName;
+	public void test(){
+		//i  = 1;i身为final全局变量无法修改
+		 final Integer j ;//j无初始化,则可以赋值一次;
+		 j = 1;
+		 //j = 2;//无法再次赋值
+	}
+	
+	public  void beforecompile(){
+		String a = "hello2";
+		final String b = "hello";//定义基本类型和String为final,则编译器会把b当做编译期常量使用，在编译前就分配栈内存,所以a == c true
+		String d = "hello";
 		
-		userName = "abc";
-		
-		//userName = "good";
-		
-		
+		String c = b + 2;
+		String e = d + 2;
+
+		System.out.println(a == c);
+		System.out.println(a == e);
+	}
+
+	public  String  getHello(){
+		return "hello";
+	}
+	
+	public  void afterCompile(){
+		String  a = "hello2";
+		final String b   = getHello();//需要调用方法才能确定值,故编译器不当其为编译期常量
+		String c = b + 2;
+		System.out.println(a == c);
 	}
 	
 	
+	public  void finalClass(final RunClass runClass){
+		runClass.setRunVal(runClass.getRunVal()+2);
+	}
+	
+	public void chanageClass(){
+		final RunClass runClass = new RunClass(2);
+		finalClass(runClass);
+		System.out.println(runClass.getRunVal());
+	}
+	
+	public static void main(String[] args) {
+		FinalClass finalClass = new FinalClass();
+		//finalClass.beforecompile();
+		//finalClass.afterCompile();
+		finalClass.chanageClass();
+	}
+	
 }
+
+class RunClass {
+
+	private  Integer runVal;
+	
+	RunClass(Integer runVal){
+		this.runVal = runVal;
+	}
+
+	public Integer getRunVal() {
+		return runVal;
+	}
+
+	public void setRunVal(Integer runVal) {
+		this.runVal = runVal;
+	}
+}
+
+
+
